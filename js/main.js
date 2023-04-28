8dcd8a8624e6ccc746b26e2aa0f8f179ee3b0c9b
francogomez <fe.gomez182@gmail.com>
2023-04-28 03:00:53 -0300

    tercera pre-entrega

--- js/main.js
+++ js/main.js
@@ -1,94 +1,153 @@
-// Declaración de variables
-let nombreCliente = prompt("Bienvenido a la tienda de zapatillas de skate. Por favor, ingrese su nombre:");
-let saldoCliente = Number(prompt("Por favor, ingrese su saldo en pesos argentinos:"));
-let catalogoZapatillas = [
-  {
-    marca: "Vans",
-    modelo: "Sk8-Hi",
-    precio: 8000,
-    talles: ["35", "36", "37", "38", "39", "40", "41", "42", "43", "44"]
-  },
-  {
-    marca: "Nike",
-    modelo: "SB Dunk Low",
-    precio: 12000,
-    talles: ["36", "37", "38", "39", "40", "41", "42", "43", "44", "45"]
-  },
-  {
-    marca: "Adidas",
-    modelo: "Superstar Vulc",
-    precio: 10000,
-    talles: ["35", "36", "37", "38", "39", "40", "41", "42", "43", "44"]
-  },
-  // Nuevo objeto agregado al catálogo
-  {
-    marca: "DC Shoes",
-    modelo: "Court Graffik",
-    precio: 9000,
-    talles: ["36", "37", "38", "39", "40", "41", "42", "43", "44"]
-  }
-];
-
-// Función para mostrar el catálogo de zapatillas
-function mostrarCatalogo() {
-  console.log("CATÁLOGO DE ZAPATILLAS DE SKATE");
-  for (let i = 0; i < catalogoZapatillas.length; i++) {
-    console.log(`${i + 1}. ${catalogoZapatillas[i].marca} ${catalogoZapatillas[i].modelo} - $${catalogoZapatillas[i].precio}`);
+// Clase Producto
+class Producto {
+  constructor(id, nombre, precio, img) {
+    this.id = id;
+    this.nombre = nombre;
+    this.precio = precio;
+    this.img = img;
+    this.cantidad = 1;
   }
 }
 
-// Función para realizar una compra
-function comprarZapatillas() {
-  let opcion = prompt("Ingrese el número de la zapatilla que desea comprar: \n1. Vans Sk8-Hi - $8000 \n2. Nike SB Dunk Low - $12000 \n3. Adidas Superstar Vulc - $10000");
-  let zapatilla = catalogoZapatillas[opcion - 1];
+// Crear objetos Producto
+const adidas = new Producto(1, "adidas", 40000, "img/adidas-lucas.jpg");
+const etnies = new Producto(2, "etnies", 15000, "img/etnies.jpg");
+const vans = new Producto(3, "vans", 20000, "img/vans.jpg");
+const dc = new Producto(4, "dc-kalis", 25000, "img/dc-kalis.jpg");
+const emerica = new Producto(5, "emerica", 30000, "img/emerica.jpg");
+const converse = new Producto(6, "converse", 20000, "img/converse.jpg");
+const nike = new Producto(7, "nike", 20000, "img/nike.jpg");
+const vans2 = new Producto(8, "vans-sk8", 13000, "img/vans-sk8.jpg");
 
-  if (zapatilla) {
-    let talle = prompt(`Ingrese el talle de las ${zapatilla.marca} ${zapatilla.modelo}:`);
-    if (zapatilla.talles.includes(talle)) {
-      if (saldoCliente >= zapatilla.precio) {
-        saldoCliente -= zapatilla.precio;
-        console.log(`¡Felicidades ${nombreCliente}! Has comprado unas ${zapatilla.marca} ${zapatilla.modelo} en talle ${talle} por $${zapatilla.precio}. Tu saldo actual es $${saldoCliente}.`);
-      } else {
-        console.log(`Lo siento ${nombreCliente}, no tienes suficiente saldo para comprar las ${zapatilla.marca} ${zapatilla.modelo} en talle ${talle}.`);
-      }
-    } else {
-      console.log(`Lo siento ${nombreCliente}, el talle ${talle} no está disponible para las ${zapatilla.marca} ${zapatilla.modelo}.`);
-    }
+// Array de productos
+const productos = [adidas, etnies, vans, dc, emerica, converse, nike, vans2];
+
+// Array vacío para el carrito
+let carrito = [];
+
+//cargar carrito desde el localstorage
+
+if(localStorage.getItem("carrito")){
+  carrito = JSON.parse(localStorage.getItem("carrito"));
+}
+
+// Modificar DOM para mostrar los productos
+const contenedorProductos = document.getElementById("contenedorProductos");
+
+// Función para mostrar los productos
+const mostrarProductos = () => {
+  productos.forEach((producto) => {
+    const card = document.createElement("div");
+    card.classList.add("col-xl-3", "col-md-6");
+    card.innerHTML = `
+      <div class="card">
+        <img src="${producto.img}" class="card-img-top imgProductos" alt="${producto.nombre}">
+        <div>
+          <h5>${producto.nombre}</h5>
+          <p>${producto.precio}</p>
+          <button class="btn colorBoton" id="boton${producto.id}" >Agregar al Carrito</button>
+        </div>
+      </div>
+    `;
+    contenedorProductos.appendChild(card);
+
+    // Agregar productos al carrito
+    const boton = document.getElementById(`boton${producto.id}`);
+    boton.addEventListener("click", () => {
+      agregarAlCarrito(producto.id);
+    });
+  });
+};
+mostrarProductos();
+
+// Función agregar al carrito
+const agregarAlCarrito = (id) => {
+  const productoEnCarrito = carrito.find((producto) => producto.id === id);
+  if (productoEnCarrito) {
+    productoEnCarrito.cantidad++;
   } else {
-    console.log("La opción ingresada no es válida.");
+    const producto = productos.find((producto) => producto.id === id);
+    carrito.push(producto);
+
   }
+  calcularTotal()
+  //localstorage
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+};
+
+//mostrar el carrito de compras
+
+const contenedorCarrito = document.getElementById("contenedorCarrito");
+const verCarrito = document.getElementById("verCarrito");
+
+verCarrito.addEventListener("click", () => {
+  mostrarCarrito();
+})
+
+const mostrarCarrito = () => {
+  contenedorCarrito.innerHTML = "";
+  carrito.forEach(producto => {
+    const card = document.createElement("div");
+    card.classList.add("col-xl-3", "col-md-6");
+    card.innerHTML = `
+      <div class="card">
+        <img src="${producto.img}" class="card-img-top imgProductos" alt="${producto.nombre}">
+        <div>
+          <h5>${producto.nombre}</h5>
+          <p>${producto.precio}</p>
+          <p>${producto.cantidad}</p>
+          <button class="btn colorBoton" id="eliminar${producto.id}"> Eliminar </button>
+        </div>
+      </div>
+    `;
+    contenedorCarrito.appendChild(card)
+
+    //eliminar productos del carrito
+    const boton = document.getElementById(`eliminar${producto.id}`);
+    boton.addEventListener("click", () => {
+      eliminarDelCarrito(producto.id);
+    })
+  })
+  calcularTotal()
+}
+
+//funcion para eliminar producto del carrito
+
+const eliminarDelCarrito = (id) => {
+  const producto = carrito.find(producto => producto.id === id);
+  const indice = carrito.indexOf(producto);
+  carrito.splice(indice, 1);
+  mostrarCarrito();
+
+  //localstorage
+  localStorage.setItem("carrito", JSON.stringify(carrito));
 }
 
 
-// Mostrar el catálogo de zapatillas
-mostrarCatalogo();
+//total de la compra
 
-// Bucle para realizar compras
-while (saldoCliente > 0) {
-  let opcionCompra = prompt("¿Desea comprar alguna zapatilla? (S/N)");
-  if (opcionCompra === "S" || opcionCompra === "s") {
-    let opcionZapatilla = prompt("Ingrese el número de la zapatilla que desea comprar:\n1. Vans Sk8-Hi - $8000 \n2. Nike SB Dunk Low - $12000 \n3. Adidas Superstar Vulc - $10000");
-    let zapatilla = catalogoZapatillas[opcionZapatilla - 1];
-    if (zapatilla) {
-      let opcionTalle = prompt(`Ingrese el talle que desea para las ${zapatilla.marca} ${zapatilla.modelo}: ${zapatilla.talles.join(", ")}`);
-      if (zapatilla.talles.includes(opcionTalle)) {
-        if (saldoCliente >= zapatilla.precio) {
-          saldoCliente -= zapatilla.precio;
-          console.log(`¡Felicidades ${nombreCliente}! Has comprado unas ${zapatilla.marca} ${zapatilla.modelo} en talle ${opcionTalle} por $${zapatilla.precio}. Tu saldo actual es $${saldoCliente}.`);
-          zapatilla.talles.splice(zapatilla.talles.indexOf(opcionTalle), 1);
-        } else {
-          console.log(`Lo siento ${nombreCliente}, no tienes suficiente saldo para comprar las ${zapatilla.marca} ${zapatilla.modelo} en talle ${opcionTalle}.`);
-        }
-      } else {
-        console.log(`Lo siento ${nombreCliente}, el talle ingresado no está disponible para las ${zapatilla.marca} ${zapatilla.modelo}.`);
-      }
-    } else {
-      console.log("La opción ingresada no es válida.");
-    }
-  } else if (opcionCompra === "N" || opcionCompra === "n") {
-    console.log(`Gracias por visitar la tienda, ${nombreCliente}. Tu saldo final es $${saldoCliente}.`);
-    break;
-  } else {
-    console.log("La opción ingresada no es válida.");
-  }
+const total = document.getElementById("total");
+
+const calcularTotal = () => {
+  let totalCompra = 0;
+  carrito.forEach(producto => {
+    totalCompra += producto.precio * producto.cantidad;
+  })
+  total.innerHTML = `total: $${totalCompra}`;
+}
+
+
+//vaciar el carrito
+
+const vaciarCarrito = document.getElementById("vaciarCarrito");
+
+vaciarCarrito.addEventListener("click", () => {
+  eliminarTodoElCarrito();
+});
+const eliminarTodoElCarrito = () => {
+  carrito = [];
+  mostrarCarrito();
+
+  //localstorage
+  localStorage.clear();
 }
